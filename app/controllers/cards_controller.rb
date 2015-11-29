d5215f000de40f486a704d01645439e3594fdf82
Aleksandr Vest <swest2462@gmail.com>
2015-11-29 12:23:22 +0300

    added review date in right format

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,11 @@
 class CardsController < ApplicationController
-def index
-  @cards = Card.all
-end
-def show
-  @card = Card.find(params[:id])
-end
+
+  def index
+    @cards = Card.all
+  end
+
+  def show
+    @card = Card.find(params[:id])
+  end
+
 end
