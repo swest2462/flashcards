ff83126a5a3b7339a6a9ccce35a0ce2f7e763f5b
Aleksandr Vest <swest2462@gmail.com>
2015-12-01 15:57:45 +0300

    removed 3 files (validator.rb)

--- app/models/validator.rb
+++
@@ -1,8 +0,0 @@
-class MyValidator < ActiveModel::Validator
-  def validate(original,translated)
-    unless :original_text == :translated_text
-      record.errors[:original_text, :translated_text] << ‘Must be uniq’
-    end
-  end
-end
-
