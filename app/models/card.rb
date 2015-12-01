364db0fda717394fdb4be7da90bf580418c5705f
Aleksandr Vest <swest2462@gmail.com>
2015-12-01 15:06:36 +0300

    added callback for +3 days in review_date

--- app/models/card.rb
+++ app/models/card.rb
@@ -4,6 +4,13 @@ class Card < ActiveRecord::Base
 
   validate :translated_properly
 
+  after_validation :days3
+
+  def days3
+    self.review_date = Time.now + 3.days
+  end
+
+
   def translated_properly
     if original_text.mb_chars.downcase == translated_text.mb_chars.downcase
      errors.add(:translated_text, 'words must be uniq')
