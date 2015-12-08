37fa2a93b9cda06666bd84dd301400115be34891
Aleksandr Vest <swest2462@gmail.com>
2015-12-08 15:24:30 +0300

    add another test

--- spec/translated_properly_spec.rb
+++ spec/translated_properly_spec.rb
@@ -9,9 +9,18 @@ RSpec.describe Card, :type => :model do
 
   describe ('review date must increased') do
     it 'inreasing date' do
-      card = Card.create(original_text: "test", translated_text: "test")
-      card.days3
+      card = Card.create(original_text: "word", translated_text: "another word", review_date: "01.01.1900")
       expect(card.review_date).to eq (Date.today + 3.days)
     end
   end
+
+  describe ('wrong word did not pass') do
+    it 'wrong word' do
+      card = Card.create(original_text: "word", translated_text: "word", review_date: "01.01.1900")
+
+      expect(Card.count).to eq 0
+    end
+  end
+
+
 end
