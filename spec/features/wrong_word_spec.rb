eb60028ce5d5183cfa400098d5ede19b4b4f1517
Aleksandr Vest <swest2462@gmail.com>
2015-12-10 15:53:27 +0300

    tying to fix test

--- spec/features/wrong_word_spec.rb
+++ spec/features/wrong_word_spec.rb
@@ -6,8 +6,7 @@ describe 'test with wrong word' do
     Card.create(original_text: "word", translated_text: "slovo", review_date:"01.01.1900")
     visit root_path
     fill_in 'user_answer', with: 'wrong word'
-    save_and_open_page
-    click_button 'Проверить!'
+    click_button 'Check!'
     expect(page).to have_content 'not right'
   end
 end
