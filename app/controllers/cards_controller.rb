4e291c08e470e0a0a6f303903302db0c65380881
Aleksandr Vest <swest2462@gmail.com>
2015-12-12 12:44:51 +0300

    fixing cards_controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,11 @@
 class CardsController < ApplicationController
 
   def index
-    @card = Card.cards_older_today.first
+    @card = current_user.cards.cards_older_today.first
   end
 
   def all
-    @card = Card.all
+    @card = current_user.cards.all
   end
 
   def show
@@ -17,11 +17,11 @@ class CardsController < ApplicationController
   end
 
   def edit
-    @card = Card.find(params[:id])
+    @card = current_user.cards.find(params[:id])
   end
 
   def create
-    @card = Card.new(card_params)
+    @card = current_user.cards.new(card_params)
 
     if @card.save
       redirect_to cards_path
@@ -62,6 +62,6 @@ class CardsController < ApplicationController
 
 private
   def card_params
-    params.require(:card).permit(:original_text, :translated_text, :review_date)
+    params.require(:card).permit(:original_text, :translated_text, :review_date, :user_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211133804) do
+ActiveRecord::Schema.define(version: 20151211162641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,10 @@ ActiveRecord::Schema.define(version: 20151211133804) do
     t.datetime "review_date"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "user_id"
   end
+
+  add_index "cards", ["user_id"], name: "index_cards_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                        null: false
@@ -43,4 +46,5 @@ ActiveRecord::Schema.define(version: 20151211133804) do
   add_index "users", ["last_logout_at", "last_activity_at"], name: "index_users_on_last_logout_at_and_last_activity_at", using: :btree
   add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
 
+  add_foreign_key "cards", "users"
 end
